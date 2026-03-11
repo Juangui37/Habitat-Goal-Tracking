63c95bce386b058650d2cec474496c2d20670ddd
Juangui37 <76864633+Juangui37@users.noreply.github.com>
2026-03-10 21:42:49 -0400

    Refactor Firebase configuration and imports
    Removed unused imports and simplified Firebase initialization.

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,18 @@
-// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-import { initializeApp } from "firebase/app";	
-import { getFirestore } from "firebase/firestore";	
-import { getAuth, GoogleAuthProvider, indexedDBLocalPersistence, initializeAuth } from "firebase/auth";	
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyDUfiByr3_sKWYtHfrPfuG83T-R0BmXvXw",
+  authDomain: "life-dashboard-e8a35.firebaseapp.com",
+  projectId: "life-dashboard-e8a35",
+  storageBucket: "life-dashboard-e8a35.firebasestorage.app",
+  messagingSenderId: "36128445674",
+  appId: "1:36128445674:web:a8dac0aa49b424ca8cf393"
+};
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {	
-  apiKey: "AIzaSyDUfiByr3_sKWYtHfrPfuG83T-R0BmXvXw",	
-  authDomain: "life-dashboard-e8a35.firebaseapp.com",	
-  projectId: "life-dashboard-e8a35",	
-  storageBucket: "life-dashboard-e8a35.firebasestorage.app",	
-  messagingSenderId: "36128445674",	
-  appId: "1:36128445674:web:a8dac0aa49b424ca8cf393"	
-};	
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);	
-export const auth = initializeAuth(app, {
-  persistence: indexedDBLocalPersistence
-});	
-export const googleProvider = new GoogleAuthProvider();	
-googleProvider.setCustomParameters({ prompt: "select_account" });	
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
